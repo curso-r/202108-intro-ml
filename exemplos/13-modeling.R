e7e74796d4153dd4f50afd6a15cbccc19b10921b
Daniel Falbel <dfalbel@gmail.com>
2021-09-27 18:53:23 -0300

    update final do modelo

--- exemplos/13-modeling.R
+++ exemplos/13-modeling.R
@@ -1,3 +1,5 @@
+
+# Pacotes -----------------------------------------------------------------
 library(tidymodels)
 library(tidyverse)
 library(rpart)
@@ -7,25 +9,29 @@ library(vip)
 
 # import ------------------------------------------------------------------
 
+ga <- readr::read_csv("ga_train.csv")
 
 # Bases de dados ----------------------------------------------------------
 
-ga_dataset <- readRDS("ga_dataset.rds")
-glimpse(ga_dataset) # German Risk
-
-ga_dataset %>% count(target)
+glimpse(ga)
+ga %>% count(comprou)
 
 # Base de treino e teste --------------------------------------------------
 
-set.seed(1)
-ga_initial_split <- initial_split(ga_dataset, strata = "target", prop = 0.75)
+ga_initial_split <- make_splits(
+  ind = list(
+    analysis = which(!as.character(ga$month) %in% c("2018-01-01", "2018-02-01")),
+    assessment = which(as.character(ga$month) %in% c("2018-01-01", "2018-02-01"))
+  ),
+  data = ga
+)
 
 ga_train <- training(ga_initial_split)
-ga_test  <- testing(ga_initial_split)
+ga_valid <- testing(ga_initial_split)
 
 # Reamostragem ------------------------------------------------------------
-
-ga_resamples <- vfold_cv(ga_train, v = 5, strata = "target")
+ga_resamples <- sliding_period(ga_train, index = month, period = "month",
+                               lookback = 5, step=2)
 
 # Exploratória ------------------------------------------------------------
 
@@ -40,9 +46,25 @@ ga_resamples <- vfold_cv(ga_train, v = 5, strata = "target")
 
 ## Data prep
 
-ga_dt_recipe <- recipe(target ~ ., data = ga_train) %>%
+ga_dt_recipe <- recipe(comprou ~ ., data = ga_train) %>%
+  update_role(month, new_role = "date") %>%
+  update_role(fullVisitorId, new_role = "id") %>%
+  step_rm(skip = TRUE,
+          last_region,
+          last_metro,
+          last_city,
+          last_networkDomain,
+          last_source,
+          last_browser
+  ) %>%
+  themis::step_downsample(comprou, under_ratio = 10) %>%
   step_novel(all_nominal_predictors()) %>%
-  step_zv(all_predictors())
+  step_zv(all_predictors()) %>%
+  step_other(
+    last_subContinent,
+    last_operatingSystem
+  )
+
 
 ## Modelo
 
@@ -66,7 +88,7 @@ grid_dt <- grid_random(
   cost_complexity(c(-9, -2)),
   tree_depth(range = c(5, 15)),
   min_n(range = c(20, 40)),
-  size = 20
+  size = 3
 )
 
 # doParallel::registerDoParallel(4)
@@ -75,7 +97,8 @@ ga_dt_tune_grid <- tune_grid(
   ga_dt_wf,
   resamples = ga_resamples,
   grid = grid_dt,
-  metrics = metric_set(roc_auc)
+  metrics = metric_set(roc_auc),
+  control = control_grid(verbose = TRUE)
 )
 
 # doParallel::stopImplicitCluster()
@@ -96,30 +119,30 @@ ga_test_preds <- collect_predictions(ga_dt_last_fit) %>% mutate(modelo = "dt")
 ## roc
 ga_test_preds %>%
   group_by(modelo) %>%
-  roc_curve(target, .pred_bad) %>%
+  roc_curve(comprou, `.pred_não`) %>%
   autoplot()
 
-## lift
-ga_test_preds %>%
-  group_by(modelo) %>%
-  lift_curve(target, .pred_bad) %>%
-  autoplot()
 
 # Variáveis importantes
 ga_dt_last_fit_model <- ga_dt_last_fit$.workflow[[1]]$fit$fit
 vip(ga_dt_last_fit_model)
 
+rpart.plot(ga_dt_last_fit_model$fit, faclen = 2)
+
 # Modelo final ------------------------------------------------------------
 
-ga_final_dt_model <- ga_dt_wf %>% fit(ga_dataset)
+ga_final_dt_model <- ga_dt_wf %>% fit(ga)
 
 
 # arquivo de submissao ----------------------------------------------------
 
+ga_test <- readr::read_csv("ga_test.csv")
+
 ga_submission <- ga_test %>%
   mutate(
-    target = predict(ga_dt_last_fit_model, new_data = ., type = "prob")$.pred
+    target = predict(ga_final_dt_model, new_data = ., type = "prob")$.pred_sim
   )
-
-write_csv(ga_submission, "ga_submission.csv")
-
+ga_submission %>%
+  mutate(fullVisitorId = paste(fullVisitorId, month, sep = "-")) %>%
+  select(fullVisitorId, comprou = target) %>%
+  write_csv("ga_submission.csv")
